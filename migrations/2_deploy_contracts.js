2b6730c86633a1fc7d3187ccc50ce7690a569151
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2021-11-13 22:37:52 +0530

    organchain contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,10 @@
-var EHR = artifacts.require("./EHR.sol");
+var EHR = artifacts.require('./EHR.sol');
+var OrganChain = artifacts.require('./OrganChain.sol');
 
 module.exports = function (deployer) {
   deployer.deploy(EHR);
 };
 
+module.exports = function (deployer) {
+  deployer.deploy(OrganChain);
+};
